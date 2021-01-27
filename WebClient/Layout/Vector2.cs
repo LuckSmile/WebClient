9411ec67c432d6c041c8f92c53f505a6043d6d20
Anton <73651390+LuckSmile@users.noreply.github.com>
2021-01-27 12:59:16 +0300

    Create Vector2.cs

---
+++ WebClient/Layout/Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient.Layout
+{
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static Vector2 Zero => new Vector2(0f, 0f);
+    }
+}
