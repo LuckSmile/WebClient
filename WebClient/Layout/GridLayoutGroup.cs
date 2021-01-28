37f134d4c1aafe552b6eab4e028ae012582f9bc8
Anton <73651390+LuckSmile@users.noreply.github.com>
2021-01-28 10:41:52 +0300

    Update GridLayoutGroup.cs
    Fixed bugs

--- WebClient/Layout/GridLayoutGroup.cs
+++ WebClient/Layout/GridLayoutGroup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -38,16 +40,16 @@ namespace WebClient.Layout
             set
             {
                 cellSize = value;
-                for(int index = 0; index < childs.Count; index++)
-                {
-                    Grid grid = childs[index];
-                    grid.MinHeight = cellSize.y;
-                    grid.MinWidth = cellSize.x;
-                }
                 for(int index = 0; index < panels.Count; index++)
                 {
                     StackPanel panel = panels[index];
                     panel.MinHeight = cellSize.y;
+                    for (int indexChild = 0; indexChild < panel.Children.Count; indexChild++)
+                    {
+                        Grid grid = panel.Children[indexChild] as Grid;
+                        grid.MinHeight = cellSize.y;
+                        grid.MinWidth = cellSize.x;
+                    }
                 }
             }
         }
@@ -57,15 +59,14 @@ namespace WebClient.Layout
             set
             {
                 spacing = value;
-                for (int index = 0; index < childs.Count; index++)
-                {
-                    Grid grid = childs[index];
-                    grid.Margin = new System.Windows.Thickness(spacing.x, 0, 0, 0);
-                }
                 for (int index = 0; index < panels.Count; index++)
                 {
                     StackPanel panel = panels[index];
-                    panel.Margin = new System.Windows.Thickness(0, spacing.y, 0, 0);
+                    panel.Margin = new Thickness(0, spacing.y, 0, 0);
+                    for(int indexChild = 0; indexChild < panel.Children.Count; indexChild++)
+                    {
+                        (panel.Children[indexChild] as Grid).Margin = new Thickness(spacing.x / 2f, 0, spacing.x / 2f, 0);
+                    }
                 }
             }
         }
@@ -75,39 +76,57 @@ namespace WebClient.Layout
             set
             {
                 constraint = value;
-                
-                int count = Convert.ToInt32(Math.Ceiling(childs.Count / constraint * 1f));
-                
-                if(count > panels.Count)
+                int count = Convert.ToInt32(Math.Ceiling(childs.Count / (constraint * 1f)));
+
+                for (int index = 0; index < panels.Count; index++)
+                {
+                    StackPanel panel = panels[index];
+                    for (int indexChild = 0; indexChild < panel.Children.Count; indexChild++)
+                    {
+                        Grid grid = panel.Children[indexChild] as Grid;
+                        panel.Children.Remove(grid);
+                        indexChild--;
+                    }
+                }
+                if (count > panels.Count)
                 {
                     for (int index = 0; index < count - panels.Count; index++)
                         CreatePanel();
                 }
                 else
                 {
-                    for (int index = 0; index < panels.Count - count; index++)
-                        panel.Children.Remove(panels[index]);
+                    for (int index = panels.Count - 1; index < panels.Count - count; index--)
+                    {
+                        RemovePanel(panels[index]);
+                        index--;
+                    }
                 }
 
+                List<Grid> grids = new List<Grid>();
+                grids.AddRange(this.childs.ToArray());
                 for(int index = 0; index < panels.Count; index++)
                 {
+                    if (grids.Count == 0)
+                        break;
                     StackPanel panel = panels[index];
-                    panel.Children.Clear();
+                    for(int indexChild = 0; indexChild < constraint; indexChild++)
+                    {
+                        Grid grid = grids[0];
+                        panel.Children.Add(grid);
+                        grids.RemoveAt(0);
+                        if (grids.Count == 0)
+                            break;
+                    }
                 }
-                
-                for (int index = 0; index < childs.Count; index++)
-                {
-                    StackPanel panel = panels[index / constraint];
-                    Grid grid = childs[index];
-                    panel.Children.Add(grid);
-                }
+                this.CellSize = cellSize;
+                this.Spacing = spacing;
             }
         }
 
         private Vector2 cellSize = Vector2.Zero;
         private Vector2 spacing = Vector2.Zero;
 
-        private int constraint = 1;
+        private int constraint = 2;
         
         private System.Windows.HorizontalAlignment horizontalAlignment = System.Windows.HorizontalAlignment.Center;
 
@@ -129,9 +148,8 @@ namespace WebClient.Layout
         {
             grid.MinHeight = cellSize.y;
             grid.MinWidth = cellSize.x;
-            
-            if(selected.Children.Count > 0)
-                grid.Margin = new System.Windows.Thickness(spacing.x, 0, 0, 0);
+
+            grid.Margin = new System.Windows.Thickness(spacing.x / 2f, 0, spacing.x / 2f, 0);
 
             childs.Add(grid);
             selected.Children.Add(grid);
@@ -155,6 +173,12 @@ namespace WebClient.Layout
             panels.Add(answer);
 
             return answer;
+        }
+        private void RemovePanel(StackPanel panel)
+        {
+            panel.Children.Remove(panel);
+            panels.Remove(panel);
+            MessageBox.Show("" + panel.Children.Count);
         }
     }
 }
