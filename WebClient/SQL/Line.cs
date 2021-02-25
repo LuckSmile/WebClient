4f4ec4ae21fa4cd2a7db1458880ef76da083af10
Anton <73651390+LuckSmile@users.noreply.github.com>
2021-02-25 11:55:55 +0300

    Create Line.cs

---
+++ WebClient/SQL/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    public class Line
+    {
+        public Dictionary<string,Column> Columns { get; private set; }
+        public Line(Column[] columns)
+        {
+            Columns = new Dictionary<string, Column>();
+            for(int index = 0; index < columns.Length; index++)
+            {
+                Column column = columns[index];
+                Columns.Add(column.Title, column);
+            }
+        }
+    }
+}
